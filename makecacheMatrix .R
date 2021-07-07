d171c9a2f786c2db050faca92915e4cb21d21d0d
Rami-QEREM <rami_qerem@yahoo.com>
2021-07-07 15:23:41 +0400

    R assignment

---
+++ makecacheMatrix .R
@@ -0,0 +1,16 @@
+makecachematrix <- function(x = matrix()) {
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+
+
